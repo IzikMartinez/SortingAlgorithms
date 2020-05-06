1ff6bec684318865a22d687de986b8b3360aab0e
jjacoel <jordanjacoel@gmail.com>
2020-05-05 23:09:48 -0500

    Added code to driver.py to create 11 arrays, sort them and plot the time taken to sort vs their size.

--- driver.py
+++ driver.py
@@ -1,8 +1,9 @@
-import pandas
+import pandas as pd
 import insert
 import MergeSort
 import random
 import time
+import matplotlib.pyplot as plt
 
 startTime = time.time()
 
@@ -25,16 +26,31 @@ def make_array(size, width):
         array[i] = random.randrange(width)
     return array
 
+#Declare arrays to hold the arrays to be sorted, their respective size, and the sort time
+arrays = []
+n = []
+times = []
 
-list1 = make_array(10, 50)
-display_list(list1)
+#Create 11 arrays of a random size from 10 to 100
+for x in range(11):
+   # print("Array ", x+1, ":")
+    n.append(random.randrange(1000,100000,1))
+    arrays.append(make_array(n[x], 100))
+    #display_list(arrays[x])
 
+#Sort all of the arrays and save the time taken to sort
 print("Merge Sort")
-MergeSort.merge_sort(list1)
+for x in range(11):
+    startTime = time.time()
+    MergeSort.merge_sort(arrays[x])
+    times.append(time.time() - startTime)
+    #print("Array ", x + 1, ":")
+    #display_list(arrays[x])
 
-display_list(list1)
+#Store the array size and sort times in a pandas data frame
+df = pd.DataFrame(list(zip(n, times)), columns = ['Size', 'Sort Time'])
+print(df)
 
-endTime = time.time() - startTime
-
-print(endTime)
-
+#Plot the size vs sort time
+df.plot(x = 'Size', y = 'Sort Time', kind = 'scatter')
+plt.show()
