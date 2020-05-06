7b01a193684083daef43b07c3e9ff8ac87a499d0
jjacoel <jordanjacoel@gmail.com>
2020-05-06 00:55:21 -0500

    Added code to plot worst case insertion and merge sorts

--- driver.py
+++ driver.py
@@ -31,6 +31,8 @@ unsorted_arrays = []
 n = []
 merge_times = []
 insertion_times = []
+WC_merge_times = []
+WC_insertion_times = []
 
 #Create 11 arrays of a random size from 1000 to 100000
 for x in range(11):
@@ -38,7 +40,6 @@ for x in range(11):
     unsorted_arrays.append(make_array(n[x], 100))
 
 sorted_arraysM = unsorted_arrays.copy()
-
 #Sort all of the arrays using merge sort and save the time taken to sort
 print("Merge Sort")
 for x in range(11):
@@ -46,13 +47,13 @@ for x in range(11):
     MergeSort.merge_sort(sorted_arraysM[x])
     merge_times.append(time.time() - startTime)
 
+sorted_arryasI = unsorted_arrays.copy()
 #Sort all of the arrays using insertion sort and save the time taken to sort
 print("Insertion Sort")
 for x in range(11):
     startTime = time.time()
-    InertionSort.insertion_sort(unsorted_arrays[x])
+    InertionSort.insertion_sort(sorted_arryasI[x])
     insertion_times.append(time.time() - startTime)
-
 
 #Store the array size and merge sort times in a pandas data frame
 merge_df = pd.DataFrame(list(zip(n, merge_times)), columns = ['Size', 'Merge Sort Time'])
@@ -70,4 +71,35 @@ plt.show()
 #Plot the size vs insertion sort time
 insertion_df.plot(x = 'Size', y = 'Insertion Sort Time', kind = 'scatter')
 plt.title('Insertion Sort Analysis')
+plt.show()
+
+#Worst Case Merge Sort
+#Get the time taken for merge sort in worst case scenario
+print("Worst Case Merge Sort")
+for x in range(11):
+    startTime = time.time()
+    WC_merge_times.append(MergeSort.worst_case(n[x]))
+
+#Get the time taken for insertion sort in worst case scenario
+print("Worst Case Insertion Sort")
+for x in range(11):
+    startTime = time.time()
+    WC_insertion_times.append(InertionSort.worst_case(n[x]))
+
+#Store the array size and worst case merge sort times in a pandas data frame
+WCmerge_df = pd.DataFrame(list(zip(n, WC_merge_times)), columns = ['Size', 'WC Merge Sort Time'])
+print(WCmerge_df)
+
+#Store the array size and worst case insertion sort times in a pandas data frame
+WCinsertion_df = pd.DataFrame(list(zip(n, WC_insertion_times)), columns = ['Size', 'WC Insertion Sort Time'])
+print(WCinsertion_df)
+
+#Plot the size vs worst case merge sort time
+WCmerge_df.plot(x = 'Size', y = 'WC Merge Sort Time', kind = 'scatter')
+plt.title('Worst Case Merge Sort Analysis')
+plt.show()
+
+#Plot the size vs worst case insertion sort time
+WCinsertion_df.plot(x = 'Size', y = 'WC Insertion Sort Time', kind = 'scatter')
+plt.title('Worst Case Insertion Sort Analysis')
 plt.show()
